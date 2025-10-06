9339b42ccd06f8e710a908c7896069c727a040f1
Savine Martens <savine@live.nl>
2025-10-06 08:51:10 +0200

    start of files

--- normal_hearing.py
+++ normal_hearing.py
@@ -1,7 +1,6 @@
 import numpy as np
-imort matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from brucezilany import stimulus, Neurogram, Species
 import brucezilany
 import os
 import glob
-import matplotlib.pyplot as plt
