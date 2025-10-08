1e2dcaaffdb6abcd4520c711a7367043fbb6c5f2
Savine Martens <savine@live.nl>
2025-10-08 09:31:55 +0200

    not bool in length

--- Hamacher_utils.py
+++ Hamacher_utils.py
@@ -109,7 +109,7 @@ def compute_internal_representation(neurogram,
     num_fibers, num_trials, num_samples = neurogram.get_output().shape
     spike_matrix = neurogram.get_output().mean(axis=1) # average across trials
     # if still in 3 dimensions
-    if len(spike_matrix.shape == 3):
+    if len(spike_matrix.shape) == 3:
         spike_matrix = np.squeeze(spike_matrix)
 
     SR, _, _, edge_frequency_critical_bands = select_critical_bands(spike_matrix, fiber_frequencies, type=critical_band_type, num_critical_bands=num_critical_bands, number_of_fibers = number_of_fibers)
