EH scaling fixed

EH_3AFC.py
@@ -19,8 +19,8 @@ temperature = 20
 dir_to_loop = './MP/EH/IR/'
 
 # get IR
-R_name = glob.glob(os.path.join(dir_to_loop, '*masker_reference1_65_*.npy'))[0]
-RT_max_name = glob.glob(os.path.join(dir_to_loop, '*masker_reference91_65dB_probe_65dB*.npy'))[0]
+R_name = glob.glob(os.path.join(dir_to_loop, '*masker_reference1_rel*.npy'))[0]
+RT_max_name = glob.glob(os.path.join(dir_to_loop, '*masker_reference1_*probe_0*.npy'))[0]
 
 IR_R = np.load(R_name)
 IR_RT_max = np.load(RT_max_name)
@@ -30,12 +30,13 @@ S = IR_RT_max - IR_R
 files = glob.glob(dir_to_loop + '*.npy')
 files.remove(R_name)
 
-scaling_factor_sigma_list = np.arange(0.2, 2.2, 0.2) 
-temperature_list = [0.00002, 0.0002, 0.002, 0.02, 0.2, 2]
+scaling_factor_sigma_list = [0.2, 0.4]#np.arange(0.2, 2.2, 0.2) 
+temperature_list = [0.00002, 0.0002] #[0.00002, 0.0002, 0.002, 0.02, 0.2, 2]
 
-save_dir_figure = './output/MP/NH/figures/'
-save_dir_results = './output/MP/NH/results/'
+save_dir_figure = './output/MP/EH/figures/'
+save_dir_results = './output/MP/EH/results/'
 
+collected = plt.figure()
 
 for folder in [save_dir_figure, save_dir_results]:
     if not os.path.exists(folder):
@@ -53,7 +54,7 @@ for scaling_factor_sigma in scaling_factor_sigma_list:
         for f, file in enumerate(files):
             print(file)
             IR_RT = np.load(file)
-            dB = int(file[file.index('probe_') + len('probe_'): file.index('dB_IR')]) - 65
+            dB = int(file[file.index('probe_') + len('probe_'): file.index('dB_rel')]) 
             dB_list.append(dB)
             percentage_correct_memory = iterate_3AFC_memory_softmax_correlation(IR_RT, IR_R, S, sigma_w, temperature, measure='pearson', n_iter=100, use_De=False, norm_bool=False, use_differences = True)           
             percentage_correct_memory_matrix[f] = percentage_correct_memory
@@ -97,12 +98,12 @@ for scaling_factor_sigma in scaling_factor_sigma_list:
         dict_pd.columns = dict_pd.iloc[0]
         dict_pd = dict_pd.drop(dict_pd.index[[0]])
 
-        single_run.savefig(save_dir_figure + '/3AFC_memory_soft_sigmaSF_' + str(scaling_factor_sigma)+ '_temp_' + str(temperature) + '.png')            
-        np.save(save_dir_results + '/3AFC_memory_soft_sigmaSF_' + str(scaling_factor_sigma) + '_temp_' + str(temperature) + '.npy', dict)
+        single_run.savefig(save_dir_figure + '/3AFC_sigmaSF_' + str(scaling_factor_sigma)+ '_temp_' + str(temperature) + '.png')            
+        np.save(save_dir_results + '/3AFC_sigmaSF_' + str(scaling_factor_sigma) + '_temp_' + str(temperature) + '.npy', dict)
 
         # both in one fig
-        collected = plt.figure()
-        plt.subplot(2,1,1)
+        plt.figure(collected)
+        plt.subplot(1,2,1)
         plt.scatter(x=dB_list, y=y_list_memory, label=f'T: {temperature}, sigma: {scaling_factor_sigma}')
         try:
             plt.plot(sorted_x, y_sig_memory, color='blue')  
@@ -114,8 +115,8 @@ for scaling_factor_sigma in scaling_factor_sigma_list:
         plt.ylabel('Percentage correct [%]')
         plt.ylim((30, 100))
         plt.xlim((min(dB_list)-1, max(dB_list)+1))
-        plt.subplot(2,1,2)
-        plt.scatter(x=dB_list, y=y_list_old, label=f'T: {temperature}, sigma: {scaling_factor_sigma}')
+        plt.subplot(1,2,2)
+        plt.scatter(x=dB_list, y=y_list_old, label=f'T: {temperature}, sigma: {scaling_factor_sigma}', color='red')
         try:
             plt.plot(sorted_x, y_sig_old, color='red')
         except:

create_signals.py
@@ -60,6 +60,80 @@ carrier_frequency = 1500 # Hz
 modulation_frequency = 40 # Hz
 modulation_duration = 300e-3
 
+
+def create_AM_tone(carrier_frequency=carrier_frequency, modulation_frequency=modulation_frequency, sound_duration=300e-3, modulation_dB=0, Fs=44100, plot=False):
+    # create unmodulated tone
+    t = np.linspace(0, sound_duration, int(sound_duration*Fs))
+    unmodulated_amplitude = 1
+    unmodulated_stimulus = unmodulated_amplitude * np.sin(2 * np.pi * carrier_frequency * t)
+
+    # create modulated tone
+    modulation_depth = 1* 10**(modulation_dB/20) # m = 10^(dB/20)
+    starting_phase = -np.pi/2 # start at 0 amplitude
+    
+    modulation_stimulus = np.sin(2 * np.pi * modulation_frequency * t + starting_phase)
+    modulated_stimulus = (1 + modulation_depth * modulation_stimulus) * unmodulated_stimulus
+ 
+    # correction factor for loudness
+    amp_modulated_stimulus = 1/np.sqrt(1+ (modulation_depth**2)/2)
+    modulated_stimulus *= amp_modulated_stimulus
+
+    # ramp signal
+    modulated_stimulus = apply_gaussian_ramp(modulated_stimulus, Fs)        
+    unmodulated_stimulus = apply_gaussian_ramp(unmodulated_stimulus, Fs)
+
+    # # get in correct integer range
+    # modulated_stimulus *= np.iinfo(np.int16).max
+    # unmodulated_stimulus *= np.iinfo(np.int16).max
+
+    modulated_segment_fname = './sounds/AM/modulated_reference1_' + str(modulation_dB) + 'dB.wav'
+    unmodulated_segment_fname = './sounds/AM/unmodulated_reference1.wav'
+    # wavfile.write(modulated_segment_fname, Fs, modulated_stimulus.astype(np.int16))
+    # wavfile.write(unmodulated_segment_fname, Fs, unmodulated_stimulus.astype(np.int16))
+    wavfile.write(modulated_segment_fname, Fs, modulated_stimulus)
+    wavfile.write(unmodulated_segment_fname, Fs, unmodulated_stimulus)
+
+    t_full = np.linspace(0, len(modulated_stimulus)/Fs, len(modulated_stimulus))
+
+    # modulated_stimulus = AudioSegment.from_wav(modulated_segment_fname)
+    # unmodulated_stimulus = AudioSegment.from_wav(unmodulated_segment_fname)
+
+    # if len(modulated_stimulus.get_array_of_samples()) != len(unmodulated_stimulus.get_array_of_samples()):
+    #     modulated_stimulus, unmodulated_stimulus = fix_lengths(modulated_stimulus, unmodulated_stimulus)
+    #     print('After fixing lengths:')
+    #     print('length unmodulated', len(unmodulated_stimulus.get_array_of_samples()))
+    #     print('length modulated', len(modulated_stimulus.get_array_of_samples()))
+
+
+
+    # audio_out_file_unmodulated = './sounds/AM/unmodulated_reference1.wav'
+    # audio_out_file_modulated = './sounds/AM/modulated_reference1_' + str(modulation_depth) + 'dB.wav'
+    # unmodulated_stimulus.export(audio_out_file_unmodulated, format="wav")
+    # modulated_stimulus.export(audio_out_file_modulated, format="wav")
+
+    if plot:
+        mod_fig = plt.figure()
+        unmodulated_stimulus = np.array(unmodulated_stimulus.get_array_of_samples())
+        modulated_stimulus = np.array(modulated_stimulus.get_array_of_samples())
+        plt.plot(t_full, unmodulated_stimulus, label='unmodulated', color='blue')
+        plt.plot(t_full, modulated_stimulus, '--', label='modulated with ' + str(modulation_dB) + 'dB depth', color='red')
+        section_length = int(len(modulated_stimulus)/2)
+        rms_unmodulated = np.sqrt(np.mean(unmodulated_stimulus[:section_length]**2))
+        rms_modulated = np.sqrt(np.mean(modulated_stimulus[:section_length]**2))
+        plt.hlines(rms_unmodulated, 0, 0.3, color='blue', label='rms unmodulated')
+        plt.hlines(rms_modulated, 0, 0.3, color='red', linestyle=':', label='rms modulated')
+        # plt.xlim(0.1, 0.15)
+        max_val = 1.1* np.max(modulated_stimulus[:section_length])
+        # plt.ylim((-1*max_val, max_val))
+        plt.legend()
+
+for dB in range(21):
+    modulation_dB = -3*dB
+    print(modulation_dB)
+    create_AM_tone(carrier_frequency=carrier_frequency, modulation_frequency=modulation_frequency, sound_duration=300e-3, modulation_dB=modulation_dB, Fs=44100, plot=False)
+
+
+
 def create_AM_tone_with_reference(carrier_frequency=carrier_frequency, modulation_frequency=modulation_frequency, sound_duration=300e-3, modulation_dB=0, Fs=44100, plot=False):
     # create reference tone
     reference_dB = 91 # [dB], amplitude of 1 seems to be 
@@ -85,6 +159,10 @@ def create_AM_tone_with_reference(carrier_frequency=carrier_frequency, modulatio
     # correction factor for loudness
     amp_modulated_stimulus = 1/np.sqrt(1+ (modulation_depth**2)/2)
     modulated_stimulus *= amp_modulated_stimulus
+
+    # ramp signal
+    modulated_stimulus = apply_gaussian_ramp(modulated_stimulus, Fs)
+    unmodulated_stimulus = apply_gaussian_ramp(unmodulated_stimulus, Fs)
 
     # get in correct integer range
     modulated_stimulus *= np.iinfo(np.int16).max
@@ -119,7 +197,7 @@ def create_AM_tone_with_reference(carrier_frequency=carrier_frequency, modulatio
     t_full = np.linspace(0, len(modulated_stimulus.get_array_of_samples())/Fs, len(modulated_stimulus.get_array_of_samples()))
 
     audio_out_file_unmodulated = './sounds/AM/unmodulated_reference91.wav'
-    audio_out_file_modulated = './sounds/AM/modulated_reference91_' + str(modulation_depth) + 'dB.wav'
+    audio_out_file_modulated = './sounds/AM/modulated_reference91_' + str(modulation_dB) + 'dB.wav'
     unmodulated_stimulus.export(audio_out_file_unmodulated, format="wav")
     modulated_stimulus.export(audio_out_file_modulated, format="wav")
 
@@ -134,16 +212,16 @@ def create_AM_tone_with_reference(carrier_frequency=carrier_frequency, modulatio
         rms_modulated = np.sqrt(np.mean(modulated_stimulus[:section_length]**2))
         plt.hlines(rms_unmodulated, 0, 0.3, color='blue', label='rms unmodulated')
         plt.hlines(rms_modulated, 0, 0.3, color='red', linestyle=':', label='rms modulated')
-        # plt.xlim(0.1, 0.15)
+        plt.xlim(0.1, 0.15)
         max_val = 1.1* np.max(modulated_stimulus[:section_length])
-        # plt.ylim((-1*max_val, max_val))
+        plt.ylim((-1*max_val, max_val))
         plt.legend()
   
 
-for dB in range(21):
-    modulation_dB = -3*dB
-    print(modulation_dB)
-    create_AM_tone_with_reference(carrier_frequency=carrier_frequency, modulation_frequency=modulation_frequency, sound_duration=300e-3, modulation_dB=modulation_dB, Fs=44100, plot=True)
+# for dB in range(21):
+#     modulation_dB = -3*dB
+#     print(modulation_dB)
+#     create_AM_tone_with_reference(carrier_frequency=carrier_frequency, modulation_frequency=modulation_frequency, sound_duration=300e-3, modulation_dB=modulation_dB, Fs=44100, plot=True)
 
 
 

electric_hearing.py
@@ -33,7 +33,7 @@ for f, file in enumerate(sorted(glob.glob(raw_data_folder + '/*trains*.npy'))):
             os.makedirs(save_dir)
     np.save(os.path.join(save_dir_neurograms, os.path.basename(file).replace('spike_trains_F120', 'neurogram').replace('.npy', '') + str(num_fibers) + 'CFs.npy'), neurogram)
     np.save(os.path.join(save_dir_IR, os.path.basename(file).replace('spike_trains_F120', 'neurogram').replace('.npy', '') + str(num_fibers) + 'CFs_' + str(IR.shape[0]) + 'bands.npy'), IR)
-    x=3
+
 
 
 