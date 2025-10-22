9f05f74212fb47bce527279f4f44aad554bd2bf9
Savine Martens <savine@live.nl>
2025-10-22 14:21:15 +0200

    for MP

--- create_data.py
+++ create_data.py
@@ -147,7 +147,7 @@ def process_normal_hearing(test, create_files=False, plot_files=True, TP2_cut_of
 
 
 
-    sound_files = sorted(glob.glob(f'./sounds/{test}/*reference91_55dB.wav'))  #dB*_{dB_str}
+    sound_files = sorted(glob.glob(f'./sounds/{test}/*reference91_55.wav'))  #dB*_{dB_str}
     print(f'Found {len(sound_files)} sound files for NH.')
 
 
